31687c0db143dcecee49229a2c0f6fd47f46da18
Tristan <kraig.tri125@gmail.com>
2013-11-17 22:15:08 -0500

    Les options apparaissent tous et la zone de texte prend une bonne mesure de la zone centrale de la fenêtre. Scroll bar rajouté.

--- Gestion Entrepot/src/EntrepotGUI.java
+++ Gestion Entrepot/src/EntrepotGUI.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -5,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -18,6 +22,7 @@ public class EntrepotGUI extends JFrame
 	{
 		super();
 		panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		this.setTitle("Gestion Entrepot");
 		panel.setVisible(true);
 		this.add(panel);
@@ -26,58 +31,73 @@ public class EntrepotGUI extends JFrame
 	
 	private void Initialisation()
 	{
-		String[] produits = { "Livre", "Ordinateur" };
-		JRadioButton ajouter = new JRadioButton("Ajouter");
-		JRadioButton enlever = new JRadioButton("Enlever");
-		JRadioButton commander = new JRadioButton("Commander");
-		JButton ok = new JButton("OK");
-		JButton lister = new JButton("Lister");
-		
-		JLabel lbCode = new JLabel("Code");
-		JTextField txtCode = new JTextField(5);
-		
-		JLabel lbNom = new JLabel("Nom");
-		JTextField txtNom = new JTextField(10);
-		
-		JLabel lbAdresse = new JLabel("Adresse");
-		JTextField txtAdresse = new JTextField(20);
-		
-		JLabel lbQuantite = new JLabel("Quantité");
-		JTextField txtQuantite = new JTextField(3);
-		
+		panel.add(new Options());
 		JTextArea txtOutput = new JTextArea("teste");
+		txtOutput.setSize(this.getWidth(), this.getHeight()/2);
 		//txtOutput.setEditable(false);
 		txtOutput.setLineWrap( true );
 		txtOutput.setWrapStyleWord( true );
+		JScrollPane scroll = new JScrollPane (txtOutput, 
+				   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		
-		JComboBox<String> selectionProduit = new JComboBox<String>(produits);
-		
-		ButtonGroup group = new ButtonGroup();
-		group.add(ajouter);
-		group.add(enlever);
-		group.add(commander);
-		
-		
-		panel.add(selectionProduit);
-		panel.add(ajouter);
-		panel.add(enlever);
-		panel.add(commander);
-		panel.add(ok);
-		panel.add(lister);
-		
-		panel.add(lbCode);
-		panel.add(txtCode);
-		
-		panel.add(lbNom);
-		panel.add(txtNom);
-		
-		panel.add(lbAdresse);
-		panel.add(txtAdresse);
-		
-		panel.add(lbQuantite);
-		panel.add(txtQuantite);
-		
-		panel.add(txtOutput);
+		panel.add(scroll, BorderLayout.CENTER);
 	}
+	
+	
+	private class Options extends JPanel
+	{
+		public Options()
+		{
+			super();
+			
+			String[] produits = { "Livre", "Ordinateur" };
+			JRadioButton ajouter = new JRadioButton("Ajouter");
+			JRadioButton enlever = new JRadioButton("Enlever");
+			JRadioButton commander = new JRadioButton("Commander");
+			JButton ok = new JButton("OK");
+			JButton lister = new JButton("Lister");
+			
+			JLabel lbCode = new JLabel("Code");
+			JTextField txtCode = new JTextField(5);
+			
+			JLabel lbNom = new JLabel("Nom");
+			JTextField txtNom = new JTextField(10);
+			
+			JLabel lbAdresse = new JLabel("Adresse");
+			JTextField txtAdresse = new JTextField(20);
+			
+			JLabel lbQuantite = new JLabel("Quantité");
+			JTextField txtQuantite = new JTextField(3);
+			
+			JComboBox<String> selectionProduit = new JComboBox<String>(produits);
+			
+			ButtonGroup group = new ButtonGroup();
+			group.add(ajouter);
+			group.add(enlever);
+			group.add(commander);
+			
+			
+			this.add(selectionProduit);
+			this.add(ajouter);
+			this.add(enlever);
+			this.add(commander);
+			this.add(ok);
+			this.add(lister);
+			
+			this.add(lbCode);
+			this.add(txtCode);
+			
+			this.add(lbNom);
+			this.add(txtNom);
+			
+			this.add(lbAdresse);
+			this.add(txtAdresse);
+			
+			this.add(lbQuantite);
+			this.add(txtQuantite);
+			
+		}
+	}
+	
 
 }
