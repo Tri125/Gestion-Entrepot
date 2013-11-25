cd7e3a7604b5b98676c9d160abd8e8f9adc781c0
Tristan <kraig.tri125@gmail.com>
2013-11-25 17:33:21 -0500

    Clean le Main

--- Gestion Entrepot/src/Main.java
+++ Gestion Entrepot/src/Main.java
@@ -6,14 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Entrepot entrepot = new Entrepot("entrepot.txt");
-		//entrepot.ListerProduits();
-		//entrepot.ListerClients();
-		//entrepot.ListerCommandes();
 		EntrepotGUI gui = new EntrepotGUI(entrepot);
 		gui.setVisible(true);
 		gui.setSize(800, 600);
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//entrepot.Enregistrer();
 	}
 
 }
